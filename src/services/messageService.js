02f524bef2cf393d13cb0ca5d7bd963c5011a500
Mevizcode <mevizdaven@gmail.com>
2025-01-28 20:56:57 +0000

    update messageService.js

--- src/services/messageService.js
+++ src/services/messageService.js
@@ -23,10 +23,9 @@ export const saveMessage = async (messages) => {
         const result = await queryDb(query, values);
 
         const savedMessages = result[0];
-
-        console.log(savedMessages)
-        console.log(savedMessages.message_id)
         const messageId = savedMessages.message_id;
+
+        addMessageStatus(messageId)
 
         //notify via socket.IO
         const io = socketIOConfig.getSocketInstance();
@@ -34,13 +33,14 @@ export const saveMessage = async (messages) => {
             io.to(receiverId).emit('join_conversation', ({ senderId, receiverId }));
             io.to(`conversation_${senderId}_${receiverId}`).emit('new_message', content);
             io.to(`conversation_${receiverId}_${senderId}`).emit('new_message', content); // Also notify the other user in the conversation
+            updateMessageStatus(messageId, 'delivered');
+
             console.log(`Message sent to conversation_${senderId}_${receiverId}`, content);
-            addMessageStatus(messageId)
         } else if (groupId) {
             io.to(`group_${groupId}`).emit('join_group', ({ groupId }));
             io.to(`group_${groupId}`).emit('new_message', content);
             console.log(`Message sent to group_${groupId}`);
-            addMessageStatus(messageId)
+            updateMessageStatus(messageId, 'delivered');
         }
 
         return savedMessages;
@@ -50,46 +50,59 @@ export const saveMessage = async (messages) => {
     }
 }
 
-export const fetchMessages = async (senderId, receiverId, groupId, startDate, endDate, page = 0, pageSize = 20) => {
+export const fetchMessages = async (senderId, receiverId, groupId, startDate, endDate, page = 1, pageSize = 20) => {
     if (!senderId || !(receiverId || groupId)) {
         throw new Error('Invalid Id');
     }
+
+    const validPage = Math.max(parseInt(page) || 1, 1); // Default to 1 if invalid
+    const validPageSize = Math.max(parseInt(pageSize) || 20, 1); // Default to 20 if invalid
+
+    const offset = (validPage - 1) * validPageSize;
 
     try {
         const cachekey = receiverId
             ? `conversation:${senderId}:${receiverId}`
             : `conversation:group:${groupId}`;
-        // 1. Check Redis cache
-        const cachedMessages = await getCache(cachekey);
-        let messages = cachedMessages ? JSON.parse(cachedMessages) : [];
 
+        // 1. Check Redis cache
+        //const cachedMessages = await getCache(cachekey);
+        //let messages = cachedMessages ? JSON.parse(cachedMessages) : [];
+        let messages = [];
         // 2. Build database query if cache is empty
-        if (!messages.length) {
+        if (!messages.length > 0) {
             let query = groupId
                 ? 'SELECT * FROM messages WHERE group_id = $1'
                 : 'SELECT * FROM messages WHERE (sender_id = $1 AND receiver_id = $2) OR (sender_id = $2 AND receiver_id = $1)';
 
             const values = groupId ? [groupId] : [senderId, receiverId];
 
+            // Conditionally add `startDate` to the query
             if (startDate) {
-                query += ' AND timestamp >= $3';
+                query += ' AND timestamp >= $' + (values.length + 1);
                 values.push(startDate);
             }
 
+            // Conditionally add `endDate` to the query
             if (endDate) {
-                query += ' AND timestamp <= $4';
+                query += ' AND timestamp <= $' + (values.length + 1);
                 values.push(endDate);
             }
 
-            query += ' ORDER BY timestamp DESC LIMIT $5 OFFSET $6';
-            values.push(pageSize, page)
+            query += ` ORDER BY timestamp ASC LIMIT $${values.length + 1} OFFSET $${values.length + 2}`;
+            values.push(validPageSize, offset);
 
             const result = await queryDb(query, values);
             messages = result;
 
-            //loop throw all the messages and update message status accordingly
+            // Update the message statuses to 'read' after fetching the messages
+            for (let message of messages) {
+                const updatedStatus = 'read';
+               await updateMessageStatus(message.message_id, updatedStatus);
+            }
+
             //Cache the result for future requests
-            await setCache(cachekey, 500, JSON.stringify(messages));
+            //await setCache(cachekey, JSON.stringify(messages));
         }
         return messages;
     } catch (err) {
